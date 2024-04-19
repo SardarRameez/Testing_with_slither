0a8a994814e7dd141098e7ff445d2d22b7c986df
rameez khan <sardarrameez5555@gmail.com>
2024-04-19 04:03:26 +0100

    commit

--- README.md
+++ README.md
@@ -1,41 +1,25 @@
-# Auditing with security tools slither and echinda
-Learn slither and echinda
-
 ## Technology Stack & Dependencies
 
 - Solidity (Writing Smart Contract)
 - [NodeJS](https://nodejs.org/en/) To create hardhat project and install dependencies using npm
 
 
-### 1. Clone/Download the Repository
 
 ### 2. Install Dependencies:
-```
 npm install
-```
 
-### 3. Install Slither
-- Install the solc compiler first then slither
+
+### 3. Install & run Slither
 ```
-brew tap ethereum/ethereum
-```
-```
-brew install solidity
+Using pip
+python3 -m pip install slither-analyzer
 ```
 ```
+Using git
 git clone https://github.com/crytic/slither.git && cd slither
-```
-```
-sudo python3 setup.py install
+python3 -m pip install .
 ```
 ```
 slither .
 ```
-### 4. Install and use echinda 
-- Install the solc compiler first then slither
-```
-docker pull trailofbits/eth-security-toolbox
-```
-```
-docker run -it trailofbits/eth-security-toolbox
-```
+
--- test/Lock.js
+++ test/Lock.js
@@ -6,9 +6,6 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
@@ -16,7 +13,6 @@ describe("Lock", function () {
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Lock = await ethers.getContractFactory("Lock");
@@ -73,10 +69,10 @@ describe("Lock", function () {
           deployOneYearLockFixture
         );
 
-        // We can increase the time in Hardhat Network
+
         await time.increaseTo(unlockTime);
 
-        // We use lock.connect() to send a transaction from another account
+
         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
           "You aren't the owner"
         );
@@ -87,7 +83,6 @@ describe("Lock", function () {
           deployOneYearLockFixture
         );
 
-        // Transactions are sent using the first signer by default
         await time.increaseTo(unlockTime);
 
         await expect(lock.withdraw()).not.to.be.reverted;
@@ -104,7 +99,7 @@ describe("Lock", function () {
 
         await expect(lock.withdraw())
           .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+          .withArgs(lockedAmount, anyValue); 
       });
     });
 
